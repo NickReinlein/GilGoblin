bc1a76c313852b3e388967a06f8aa756bb34a84d
Nick Reinlein <nicholasreinlein@gmail.com>
2021-12-08 06:56:32 -0500

    Refactored market prices for easier external use

--- Finance/Price.cs
+++ Finance/Price.cs
@@ -7,9 +7,9 @@ namespace GilGoblin.Finance
     internal class Price
     {
         private static Random random_gen = new Random();
-        public static Task<int> Get_Market_Price(int item_id, string world_name)
+        public static int Get_Market_Price(int item_id, string world_name)
         {
-            return Market.Get_Market_Price(item_id, world_name);
+            return Market.Get_Market_Price(item_id, world_name).GetAwaiter().GetResult();
         }
 
     }
--- Tests/Dev_Tests.cs
+++ Tests/Dev_Tests.cs
@@ -16,8 +16,9 @@ namespace GilGoblin.Tests
         public const int item_id = 5114;
         public static void test_Fetch_Market_Price()
         {
-            int marketPrice = Market.Get_Market_Price(item_id, world_name).GetAwaiter().GetResult();
+            int marketPrice = Price.Get_Market_Price(item_id, world_name);
             Console.WriteLine("Final market price: " + marketPrice);
+            Console.ReadLine();
         }
     }
 }
