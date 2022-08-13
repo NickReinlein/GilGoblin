9aea3633f75ea56d615f462636ecd7b362231a45
Nick Reinlein <nicholasreinlein@gmail.com>
2022-08-13 12:16:29 -0400

    Fixed based on warnings

--- src/pocos/MarketDataPoco.cs
+++ src/pocos/MarketDataPoco.cs
@@ -4,40 +4,55 @@ namespace GilGoblin.pocos
 {
     public class MarketDataPoco
     {
-
         // The item ID.
         public int itemID { get; set; }
+
         // The world ID, if applicable.
         public int worldID { get; set; }
+
         // The last upload time for this endpoint, in milliseconds since the UNIX epoch.
         public long lastUploadTime { get; set; }
 
         // The DC name, if applicable.
         public string? name { get; set; }
+
         // The region name, if applicable.
         public string? regionName { get; set; }
 
         // The average listing price, with outliers removed beyond 3 standard deviations of the mean.
         public decimal averageListingPrice { get; set; }
+
         // The average NQ listing price, with outliers removed beyond 3 standard deviations of the mean.
         public decimal averagePriceNQ { get; set; }
+
         // The average HQ listing price, with outliers removed beyond 3 standard deviations of the mean.
         public decimal averagePriceHQ { get; set; }
 
-        // TODO: return later to see which is the best data type for price Poco
         // The average sale price, with outliers removed beyond 3 standard deviations of the mean.
-        public float? averageSale { get; set; }
+        public float averageSale { get; set; }
+
         // The average NQ sale price, with outliers removed beyond 3 standard deviations of the mean.
-        public decimal? averageSaleNQ { get; set; }
+        public decimal averageSaleNQ { get; set; }
+
         // The average HQ sale price, with outliers removed beyond 3 standard deviations of the mean.
-        public double? averageSaleHQ { get; set; }
-        public MarketDataPoco()
-        {
-        }
+        public double averageSaleHQ { get; set; }
+
+        public MarketDataPoco() { }
+
         [JsonConstructor]
-        public MarketDataPoco(int itemID, int worldID, long lastUploadTime, string? name, string? regionName, 
-            decimal currentAveragePrice, decimal currentAveragePriceNQ, decimal currentAveragePriceHQ, 
-            float? averagePrice, decimal? averagePriceNQ, double? averagePriceHQ)
+        public MarketDataPoco(
+            int itemID,
+            int worldID,
+            long lastUploadTime,
+            string? name,
+            string? regionName,
+            decimal currentAveragePrice,
+            decimal currentAveragePriceNQ,
+            decimal currentAveragePriceHQ,
+            float averagePrice,
+            decimal averagePriceNQ,
+            double averagePriceHQ
+        )
         {
             this.itemID = itemID;
             this.worldID = worldID;
@@ -51,8 +66,6 @@ namespace GilGoblin.pocos
             this.averageSaleNQ = averagePriceNQ;
             this.averageSaleHQ = averagePriceHQ;
         }
-
-        
 
         public MarketDataPoco(MarketDataPoco copyMe)
         {
