6f49de9910f2221066e5d3ccd89bb16d95117a24
Nick Reinlein <nicholasreinlein@gmail.com>
2022-12-20 09:14:19 -0500

    Renamed controller

---
+++ src/Controller/ItemController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using GilGoblin.Pocos;
+
+namespace GilGoblin.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class ItemController : ControllerBase
+{
+    private readonly ILogger<ItemController> _logger;
+
+    public ItemController(ILogger<ItemController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<ItemInfoPoco> Get()
+    {
+        _logger.LogInformation($"Fetching all item info data");
+        return Enumerable.Range(1, 5).Select(index => Get(index)).ToArray();
+    }
+
+    [HttpGet("{id}")]
+    public ItemInfoPoco Get(int id)
+    {
+        _logger.LogInformation($"Fetching item info id: {id}");
+        return new ItemInfoPoco
+        {
+            ID = id,
+            Description = "TestItem" + id,
+            GatheringID = id + 144,
+            IconID = id + 42,
+            Name = "TestItem" + id,
+            StackSize = 1,
+            VendorPrice = id
+        };
+    }
+}
