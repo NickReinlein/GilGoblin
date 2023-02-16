defebd7883edcfce1e4817a04f2c4e9cf68a3452
Nick Reinlein <nicholasreinlein@gmail.com>
2023-02-16 17:50:11 -0500

    RecipeGateway added

--- src/Database/RecipeGateway.cs
+++ src/Database/RecipeGateway.cs
@@ -5,26 +5,45 @@ namespace GilGoblin.Database;
 
 public class RecipeGateway : IRecipeRepository
 {
-    public async Task<RecipePoco?> Get(int recipeID) => new();
+    private readonly GoblinDatabase _database;
+    private readonly ILogger<GoblinDatabase> _logger;
 
-    public async Task<IEnumerable<RecipePoco?>> GetRecipesForItem(int itemID) =>
-        Array.Empty<RecipePoco>();
+    public RecipeGateway(GoblinDatabase database, ILogger<GoblinDatabase> logger)
+    {
+        _database = database;
+        _logger = logger;
+    }
+
+    public async Task<RecipePoco?> Get(int recipeID)
+    {
+        _logger.LogInformation("Getting recipe {ID}", recipeID);
+        using var context = await GetContext();
+        var recipePoco = context?.Recipe?.FirstOrDefault(x => x.ID == recipeID);
+        return recipePoco;
+    }
+
+    public async Task<IEnumerable<RecipePoco?>> GetRecipesForItem(int itemID)
+    {
+        _logger.LogInformation("Getting recipes for item {ID}", itemID);
+        using var context = await GetContext();
+        return context?.Recipe?.Where(i => i.TargetItemID == itemID).ToList()
+            ?? new List<RecipePoco>();
+    }
 
     public async Task<IEnumerable<RecipePoco?>> GetMultiple(IEnumerable<int> recipeIDs)
     {
-        var recipes = new List<RecipePoco>();
-        foreach (var recipe in recipeIDs)
-        {
-            var result = await Get(recipe);
-            if (result is null)
-                continue;
-            recipes.Add(result);
-        }
-        return recipes;
+        _logger.LogInformation("Getting {Number} recipes", recipeIDs.Count());
+        using var context = await GetContext();
+        return context?.Recipe?.Where(i => recipeIDs.Contains(i.ID)).ToList()
+            ?? new List<RecipePoco>();
     }
 
     public async Task<IEnumerable<RecipePoco>> GetAll()
     {
-        return new List<RecipePoco>();
+        _logger.LogInformation("Getting all recipes");
+        using var context = await GetContext();
+        return context?.Recipe?.ToList() ?? new List<RecipePoco>();
     }
+
+    private async Task<GilGoblinDbContext?> GetContext() => await _database.GetContextAsync();
 }
