07a931d1398cb1e7256fcf31433eb15419b81694
Nick Reinlein <nicholasreinlein@gmail.com>
2023-02-18 16:59:35 -0500

    Reworked to naming it Api

---
+++ src/Api/Bootstrap.cs
@@ -0,0 +1,20 @@
+using GilGoblin.Api.DI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GilGoblin.Api;
+
+public static class Bootstrap
+{
+    public static IHostBuilder CreateHostBuilder(string environment, params string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .UseEnvironment(environment)
+            .ConfigureServices(
+                (hostContext, services) =>
+                {
+                    services.AddSingleton(hostContext.Configuration).AddServices();
+                }
+            );
+    }
+}
