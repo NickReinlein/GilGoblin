3aa0ba70a25e5421a33a642d3318bdfb1e0f9996
Nick Reinlein <nicholasreinlein@gmail.com>
2023-03-20 16:35:47 -0400

    Filters for coverage

--- GenerateTestCoverageCommands.sh
+++ GenerateTestCoverageCommands.sh
@@ -1,2 +1,3 @@
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --collect "Xplat Code Coverage" &&
-    reportgenerator "-reports:./tests/TestResults/**/**/*.xml" "-targetdir:./tests/CodeCoverageReports/" -reportTypes:html
+    reportgenerator "-reports:./tests/TestResults/**/**/*.xml" "-targetdir:./tests/CodeCoverageReports/" "-classfilters:-*poco*;-*exception*;-*DbContext*;-*Api.*"
+ 
