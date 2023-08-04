649627a9b0f4b54fca9cbfc2923bdcce728d1e0d
Nick Reinlein <nicholasreinlein@gmail.com>
2023-08-04 11:24:42 -0400

    Item gateway tests

--- src/Database/ItemGateway.cs
+++ src/Database/ItemGateway.cs
@@ -10,35 +10,17 @@ namespace GilGoblin.Database;
 
 public class ItemGateway : IItemRepository
 {
-    private readonly IContextFetcher _database;
-    private readonly ILogger<GilGoblinDatabase> _logger;
+    private readonly IItemRepository _recipes;
 
-    public ItemGateway(IContextFetcher database, ILogger<GilGoblinDatabase> logger)
+    public ItemGateway(IItemRepository recipes)
     {
-        _database = database;
-        _logger = logger;
+        _recipes = recipes;
     }
 
-    public async Task<ItemInfoPoco?> Get(int itemID)
-    {
-        _logger.LogInformation("Getting item {ID}", itemID);
-        using var context = await _database.GetContextAsync();
-        var itemInfoPoco = context?.ItemInfo?.FirstOrDefault(x => x.ID == itemID);
-        return itemInfoPoco;
-    }
+    public async Task<ItemInfoPoco?> Get(int recipeID) => await _recipes.Get(recipeID);
 
-    public async Task<IEnumerable<ItemInfoPoco>> GetAll()
-    {
-        _logger.LogInformation("Getting all items");
-        using var context = await _database.GetContextAsync();
-        return context?.ItemInfo?.ToList() ?? new List<ItemInfoPoco>();
-    }
+    public async Task<IEnumerable<ItemInfoPoco?>> GetMultiple(IEnumerable<int> recipeIDs) =>
+        await _recipes.GetMultiple(recipeIDs);
 
-    public async Task<IEnumerable<ItemInfoPoco?>> GetMultiple(IEnumerable<int> itemIDs)
-    {
-        _logger.LogInformation("Getting {Number} items", itemIDs.Count());
-        using var context = await _database.GetContextAsync();
-        return context?.ItemInfo?.Where(i => itemIDs.Contains(i.ID)).ToList()
-            ?? new List<ItemInfoPoco>();
-    }
+    public async Task<IEnumerable<ItemInfoPoco>> GetAll() => await _recipes.GetAll();
 }
