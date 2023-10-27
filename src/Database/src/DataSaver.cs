62f470fe9394662fe0c7b42a7f489a0901e433fa
Nick Reinlein <nicholasreinlein@gmail.com>
2023-10-26 21:23:47 -0400

    updated db package

--- src/Database/src/DataSaver.cs
+++ src/Database/src/DataSaver.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GilGoblin.Database.Pocos;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace GilGoblin.Database;
 
-public class DataSaver<T> : IDataSaver<T> where T : class
+public class DataSaver<T> : IDataSaver<T> where T : class, IIdentifiable
 {
-
     protected readonly GilGoblinDbContext DbContext;
     private readonly ILogger<DataSaver<T>> _logger;
 
@@ -16,6 +17,7 @@ public class DataSaver<T> : IDataSaver<T> where T : class
         DbContext = dbContext;
         _logger = logger;
     }
+
     public async Task<bool> SaveAsync(IEnumerable<T> updates)
     {
         var updatesList = updates.ToList();
@@ -23,7 +25,13 @@ public class DataSaver<T> : IDataSaver<T> where T : class
             return false;
 
         await using var context = DbContext;
-        await context.AddRangeAsync(updatesList);
+        foreach (var update in updatesList)
+        {
+            context.Entry(update).State =
+                update.GetId() > 0
+                    ? EntityState.Added
+                    : EntityState.Modified;
+        }
         await context.SaveChangesAsync();
         _logger.LogInformation($"Saved {updatesList.Count()} entries for type {typeof(T).Name}");
         return true;
