3fde18321fcc12e47412d3ad173b50f21ed5188e
Nick Reinlein <nicholasreinlein@gmail.com>
2023-10-29 19:29:05 -0400

    Split each batch of updates into minimum 2 batches

--- tests/Services/BatcherTests.cs
+++ tests/Services/BatcherTests.cs
@@ -34,6 +34,19 @@ public class BatcherTests
         Assert.That(result.First(), Has.Count.EqualTo(1));
     }
 
+
+    [Test]
+    public void GivenTwoEntries_WhenWeSplitIntoBatchJobs_WeReturnAListWithTwoBatchesOfOneEntry()
+    {
+        var array = new List<int> { 1, 2 };
+
+        var result = _batcher.SplitIntoBatchJobs(array);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Has.Count.EqualTo(2));
+        Assert.That(result.All(r => r.Count == 1));
+    }
+
     [Test]
     public void GivenMultipleEntries_WhenWeSplitIntoBatchJobsLessThanPageSize_WeReturnInOneElement()
     {
@@ -42,8 +55,12 @@ public class BatcherTests
         var result = _batcher.SplitIntoBatchJobs(array);
 
         Assert.That(result, Is.Not.Null);
-        Assert.That(result, Has.Count.EqualTo(1));
-        Assert.That(result.First(), Has.Count.EqualTo(3));
+        Assert.That(result, Has.Count.EqualTo(2));
+        Assert.That(result[0], Has.Count.EqualTo(1));
+        Assert.That(result[1], Has.Count.EqualTo(2));
+        var totalResult = result[0].Concat(result[1]).ToList();
+        foreach (var id in array)
+            Assert.That(totalResult, Does.Contain(id));
     }
 
     [Test]
