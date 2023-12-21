059036531f6154bcc3fcad17b36f12a49a7fa5d1
Nick Reinlein <nicholasreinlein@gmail.com>
2023-12-21 17:01:52 -0500

    wip

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     testEnvironment: 'jsdom',
     preset: 'ts-jest',
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest'
-    },
+    // transform: {
+    //     '^.+\\.jsx?$': 'babel-jest'
+    // },
     testMatch: [
         "**/__tests__/**/*.ts?(x)",
         "**/?(*.)+(spec|test).ts?(x)"
