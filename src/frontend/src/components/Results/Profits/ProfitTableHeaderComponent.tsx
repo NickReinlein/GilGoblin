2982d890f5da3961f2210ab2e2c84627c0ae00e4
Nick Reinlein <nicholasreinlein@gmail.com>
2024-01-29 18:19:00 -0500

    added sorting arrow to profit table headers (#50)
    * wip adding arrow
    
    * unicode triangle arrow success story4
    
    * unnecessary file removed, & formatting

--- src/frontend/src/components/Results/Profits/ProfitTableHeaderComponent.tsx
+++ src/frontend/src/components/Results/Profits/ProfitTableHeaderComponent.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import '../../../styles/ProfitTableHeaderComponent.css';
 
 interface ProfitTableHeaderComponentProps {
-    headers: string[];
-    onHeaderClick: (clickedHeader: string) => void;
+    headers: string[],
+    onHeaderClick: (clickedHeader: string) => void,
+    columnSort?: string,
+    ascending?: boolean
 }
 
 const ProfitTableHeaderComponent: React.FC<ProfitTableHeaderComponentProps> = (
     {
         headers,
         onHeaderClick,
+        columnSort,
+        ascending = false
     }) => {
     const handleHeaderClick = (clickedHeader: string) => {
         onHeaderClick(clickedHeader);
@@ -21,6 +25,11 @@ const ProfitTableHeaderComponent: React.FC<ProfitTableHeaderComponentProps> = (
             {headers.map((header: string) => (
                 <th key={header} onClick={() => handleHeaderClick(header)}>
                     {header}
+                    {columnSort && header === columnSort && (
+                        <span className={`sort-arrow ${ascending ? 'asc' : 'desc'}`}>
+                            {ascending ? '▲' : '▼'}
+                        </span>
+                    )}
                 </th>
             ))}
         </tr>
