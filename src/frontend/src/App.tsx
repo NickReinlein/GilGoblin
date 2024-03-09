803d7c05bfdfb7d0e8c117e85944977ec8d1b665
Nick Reinlein <nicholasreinlein@gmail.com>
2024-03-09 11:25:29 -0500

    added favicon

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TabComponent from "./components/Header/TabComponent";
+import TitleComponent from "./components/Header/TitleComponent";
 
 
 const App = () => {
     return (
         <div data-testid="app-tab">
+            <TitleComponent/>
             <TabComponent/>
         </div>
     );
