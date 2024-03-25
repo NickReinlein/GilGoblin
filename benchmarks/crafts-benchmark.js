acaf6e9c2f4fcf37f52c3eb5708b330c2310a46f
Nick Reinlein <nicholasreinlein@gmail.com>
2024-03-25 16:56:28 -0400

    changing stages and basic url for testing

--- benchmarks/crafts-benchmark.js
+++ benchmarks/crafts-benchmark.js
@@ -3,22 +3,21 @@ import {check, sleep} from 'k6';
 
 export let options = {
     stages: [
-        {duration: '20s', target: 2},
-        {duration: '30s', target: 5},
-        // {duration: '1m', target: 10},
-        // {duration: '1m', target: 5},
-        // {duration: '1m', target: 2},
+        {duration: '1m', target: 2},
+        {duration: '1m', target: 5},
+        {duration: '3m', target: 10},
+        {duration: '1m', target: 5},
+        {duration: '1m', target: 2},
     ],
 };
 
 const worldIds = [21, 22, 23, 34];
 
-export default function (delay = 5) {
+export default function (delay = 500) {
     let randomIndex = Math.floor(Math.random() * worldIds.length);
     let worldId = worldIds[randomIndex];
 
-    // let response = http.get(`http://localhost:55448/craft/${worldId}`);
-    let response = http.get(`http://host.docker.internal:55448/item/1639`);
+    let response = http.get(`http://host.docker.internal:55448/craft/${worldId}`);
 
     check(response, {
         'status is 200': (r) => r.status === 200,
