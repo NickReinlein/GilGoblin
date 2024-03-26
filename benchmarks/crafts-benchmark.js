d03210f4d749554848e7c7f7f4e7c4833a18866d
Nick Reinlein <nicholasreinlein@gmail.com>
2024-03-26 16:14:25 -0400

    k6 benchmarks successfully run and passed

--- benchmarks/crafts-benchmark.js
+++ benchmarks/crafts-benchmark.js
@@ -2,18 +2,20 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export let options = {
-    stages: [
-        {duration: '1m', target: 2},
-        {duration: '1m', target: 5},
-        {duration: '3m', target: 10},
-        {duration: '1m', target: 5},
-        {duration: '1m', target: 2},
-    ],
+    stages: [{duration: '5m', target: 50}, // Ramp-up to 50 VUs over 5 minutes
+        {duration: '5m', target: 100}, // Ramp-up to 100 VUs over 5 minutes
+        {duration: '10m', target: 100}, // Hold 100 VUs for 10 minutes
+        {duration: '5m', target: 0}, // Ramp-down to 0 VUs over 5 minutes
+    ], thresholds: {
+        http_req_duration: ['p(98)<500'], // 98% of requests must complete below 500ms
+    },
 };
 
 const worldIds = [21, 22, 23, 34];
 
-export default function (delay = 500) {
+export default function () {
+    const delay = Math.floor(Math.random() * 500) + 1000;
+
     let randomIndex = Math.floor(Math.random() * worldIds.length);
     let worldId = worldIds[randomIndex];
 
