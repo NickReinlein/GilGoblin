2e82638890bd1dafc64504710c835cd698c35e08
Nick Reinlein <nicholasreinlein@gmail.com>
2024-03-27 12:24:47 -0400

    wip k6 to prometheus

--- benchmarks/crafts-benchmark.js
+++ benchmarks/crafts-benchmark.js
@@ -1,20 +1,33 @@
-import http from 'k6/http';
-import {check, sleep} from 'k6';
+import http from "k6/http";
+import {check, sleep} from "k6";
 
 export let options = {
-    stages: [{duration: '5m', target: 50}, // Ramp-up to 50 VUs over 5 minutes
-        {duration: '5m', target: 100}, // Ramp-up to 100 VUs over 5 minutes
-        {duration: '10m', target: 100}, // Hold 100 VUs for 10 minutes
-        {duration: '5m', target: 0}, // Ramp-down to 0 VUs over 5 minutes
-    ], thresholds: {
-        http_req_duration: ['p(98)<500'], // 98% of requests must complete below 500ms
-    },
+    // stages: [{duration: '5m', target: 50}, // Ramp-up to 50 VUs over 5 minutes
+    //     {duration: '5m', target: 100}, // Ramp-up to 100 VUs over 5 minutes
+    //     {duration: '10m', target: 100}, // Hold 100 VUs for 10 minutes
+    //     {duration: '5m', target: 0}, // Ramp-down to 0 VUs over 5 minutes
+    // ],
+    ext: {
+        stages: [
+            {duration: "20s", target: 2},
+            {duration: "20s", target: 0},
+        ],
+        thresholds: {
+            http_req_duration: ["p(98)<500"], // 98% of requests must complete below 500ms
+            http_req_failed: ["rate<=0.02"], // http errors should be less than or equal to 2%
+        },
+        metricsExporters: {
+            prometheus: {
+                address: "host.docker.internal:9090",
+            },
+        },
+    }
 };
 
 const worldIds = [21, 22, 23, 34];
 
 export default function () {
-    const delay = Math.floor(Math.random() * 500) + 1000;
+    const delay = 2000;
 
     let randomIndex = Math.floor(Math.random() * worldIds.length);
     let worldId = worldIds[randomIndex];
@@ -22,7 +35,7 @@ export default function () {
     let response = http.get(`http://host.docker.internal:55448/craft/${worldId}`);
 
     check(response, {
-        'status is 200': (r) => r.status === 200,
+        "status is 200": (r) => r.status === 200,
     });
 
     sleep(delay);
