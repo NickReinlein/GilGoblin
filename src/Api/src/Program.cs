6cde0e07153f33d26cb4b2eae7477f5d8263bc15
Nick Reinlein <nicholasreinlein@gmail.com>
2024-05-28 18:31:48 -0400

    Hide secrets from dev stage (#76)
    * wip
    
    * enhancing docker to use environment variables
    
    * remove version (deprecated)
    
    * moving secrets to .env file
    
    * fixing component tests
    
    * WIP getting API done
    
    * slim down docker
    
    * remove unnecessary
    
    * more changes
    
    * revert
    
    * remove sensitive logging from dev stage

--- src/Api/src/Program.cs
+++ src/Api/src/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace GilGoblin.Api;
@@ -7,11 +8,17 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        CreateHostBuilder(args)
+            .Build()
+            .Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddEnvironmentVariables();
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
--- src/Database/src/GilGoblinDbContext.cs
+++ src/Database/src/GilGoblinDbContext.cs
@@ -27,8 +27,10 @@ public class GilGoblinDbContext : DbContext
     {
         var connectionString = Configuration.GetConnectionString(nameof(GilGoblinDbContext));
         optionsBuilder.UseNpgsql(connectionString);
-        optionsBuilder.EnableSensitiveDataLogging();
-        optionsBuilder.EnableDetailedErrors();
+
+        // Only used during development and debugging
+        // optionsBuilder.EnableSensitiveDataLogging();
+        // optionsBuilder.EnableDetailedErrors();
 
         base.OnConfiguring(optionsBuilder);
     }
@@ -115,8 +117,13 @@ public class GilGoblinDbContext : DbContext
         modelBuilder.Entity<RecipeProfitPoco>().HasKey(t => new { t.RecipeId, t.WorldId });
         modelBuilder.Entity<RecipeProfitPoco>().Property(t => t.RecipeId).HasColumnName("recipeid");
         modelBuilder.Entity<RecipeProfitPoco>().Property(t => t.WorldId).HasColumnName("worldid");
-        modelBuilder.Entity<RecipeProfitPoco>().Property(t => t.RecipeProfitVsSold).HasColumnName("profitvssold");
-        modelBuilder.Entity<RecipeProfitPoco>().Property(t => t.RecipeProfitVsListings)
+        modelBuilder
+            .Entity<RecipeProfitPoco>()
+            .Property(t => t.RecipeProfitVsSold)
+            .HasColumnName("profitvssold");
+        modelBuilder
+            .Entity<RecipeProfitPoco>()
+            .Property(t => t.RecipeProfitVsListings)
             .HasColumnName("profitvslistings");
         modelBuilder.Entity<RecipeProfitPoco>().Property(t => t.Updated).HasColumnName("updated");
     }
