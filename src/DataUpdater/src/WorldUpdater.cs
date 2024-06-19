f87c5f3ef3f83a7b8c5502ed239187c4ee6f79e9
Nick Reinlein <nicholasreinlein@gmail.com>
2024-06-19 18:45:47 -0400

    adding world updating service

--- src/DataUpdater/src/WorldUpdater.cs
+++ src/DataUpdater/src/WorldUpdater.cs
@@ -1,8 +1,92 @@
-// using GilGoblin.Database.Pocos;
-//
-// namespace GilGoblin.DataUpdater;
-//
-// public class WorldUpdater: DataUpdater<WorldPoco, WorldWebPoco>
-// {
-//     
-// }
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GilGoblin.Database;
+using GilGoblin.Database.Pocos;
+using GilGoblin.Fetcher;
+using GilGoblin.Fetcher.Pocos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GilGoblin.DataUpdater;
+
+public class WorldUpdater(IServiceScopeFactory ScopeFactory, IWorldFetcher Fetcher, ILogger<WorldUpdater> Logger)
+    : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            try
+            {
+                await GetAllWorlds(ct);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("{Service}: An exception occured during the Api call: {ErrorMessage}",
+                    nameof(WorldUpdater), ex.Message);
+            }
+
+            var delay = TimeSpan.FromMinutes(5);
+            Logger.LogInformation("{Service}: Awaiting delay of {Delay} seconds before performing next update",
+                nameof(WorldUpdater), delay.TotalSeconds);
+            await Task.Delay(delay, ct);
+        }
+    }
+
+    public async Task GetAllWorlds(CancellationToken ct)
+    {
+        var result = await FetchAllWorlds();
+        await ConvertAndSaveToDbAsync(result);
+    }
+
+    private async Task<List<WorldWebPoco>> FetchAllWorlds()
+    {
+        try
+        {
+            // TODO: do we really need this instead of using DI to inject the world fetcher?
+            // using var scope = ScopeFactory.CreateScope();
+            // var fetcher = scope.ServiceProvider.GetRequiredService<IWorldFetcher>();
+            Logger.LogInformation("Fetching updates for all worlds");
+            var timer = new Stopwatch();
+            timer.Start();
+            // var updated = await fetcher.GetAll();
+            var updated = await Fetcher.GetAll();
+            timer.Stop();
+
+            Logger.LogInformation("Received updates for {Count} worlds}", updated.Count);
+            Logger.LogInformation("Total call time: {CallTime}", timer.Elapsed.TotalMilliseconds);
+            return updated;
+        }
+        catch (Exception e)
+        {
+            Logger.LogError("Failed to fetch updates for worlds: {Message}", e.Message);
+            return [];
+        }
+    }
+
+    private async Task ConvertAndSaveToDbAsync(List<WorldWebPoco> webPocos)
+    {
+        var updateList = webPocos.ToWorldPocoList();
+        if (!updateList.Any())
+            return;
+
+        try
+        {
+            using var scope = ScopeFactory.CreateScope();
+            var saver = scope.ServiceProvider.GetRequiredService<IDataSaver<WorldPoco>>();
+            var success = await saver.SaveAsync(updateList);
+            if (!success)
+                throw new DbUpdateException($"Saving from {nameof(IDataSaver<WorldPoco>)} returned failure");
+        }
+        catch (Exception e)
+        {
+            Logger.LogError($"Failed to save {webPocos.Count} entries for {nameof(WorldPoco)}: {e.Message}");
+        }
+    }
+}
---
+++ src/Fetcher/src/Pocos/WorldWebPocoExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using GilGoblin.Database.Pocos;
+
+namespace GilGoblin.Fetcher.Pocos;
+
+public static class WorldWebPocoExtensions
+{
+    public static WorldPoco ToWorldPoco(this WorldWebPoco webPoco) =>
+        new() { Id = webPoco.GetId(), Name = webPoco.Name };
+
+    public static List<WorldPoco> ToWorldPocoList(this IEnumerable<WorldWebPoco?> pocos) =>
+        pocos.Where(poco => poco is not null).Select(world => world!.ToWorldPoco()).ToList();
+}
