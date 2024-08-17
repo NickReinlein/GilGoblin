3901552600fa4b1e06aacf6e84d1ca0efffc5bce
Nick Reinlein <nicholasreinlein@gmail.com>
2024-08-17 12:13:15 -0400

    Changed about page

--- src/frontend/src/components/Header/AboutPageComponent.tsx
+++ src/frontend/src/components/Header/AboutPageComponent.tsx
@@ -4,32 +4,31 @@ import '../../styles/AboutPageComponent.css';
 const AboutPageComponent = () => {
     return (
         <div className="about-page">
-            <h1>About GilGoblin</h1>
-            <p>
+            <div>
+                <h1>GilGoblin</h1>
                 <code>GilGoblin</code> is a project designed to calculate in-game crafting profitability in
                 <a href="https://www.finalfantasyxiv.com/"> Final Fantasy XIV (FFXIV)</a>, and display a result for
-                users sorted by profitability.
-            </p>
-            <p>
+                users sorted by profitability.<br/><br/>
                 It determines the most profitable items to craft based on current market prices, vendor prices, and
-                crafting component costs.
-            </p>
-            <p>
+                crafting component costs.<br/><br/>
                 Recent prices are stored in a local PostgresSQL database, which is refreshed by a background service.
-                This keeps profits relevant with up-to-date data.
-            </p>
-            <p>For more information, please visit the project's <a href="https://github.com/NickReinlein/GilGoblin">GitHub
-                repository</a>.
-            </p>
-            <h1>About the author</h1>
-            <p>
-                Nick Reinlein is a software developer with 9 years experience, currently working from Canada.
-                Nick has a passion for web development and automation, particularly when it involves complexity.
-                He has a dual background in Computer Science and Financial Accounting. He is married and father of 3
-                children. Nick tries to balance life, work and leisure.
-            </p>
-            <h2> Contact </h2>
-            <a href="mailto:admin@gilgoblin.com">admin@gilgoblin.com</a>
+                This keeps profits relevant with up-to-date data.<br/><br/>
+                For more information, please visit the project's <a href="https://github.com/NickReinlein/GilGoblin">GitHub
+                repository</a>.<br/>
+
+            </div>
+            <div>
+                <h2>About the author</h2>
+
+                Nick Reinlein is a software developer from Montreal, Canada.
+                Nick has a passion for web development and automation, especially when it involves complexity.
+                He has a background in Computer Science and Financial Accounting. He is married and father of 3
+                children. Nick is a lifelong fan of Final Fantasy and video games.<br/>
+            </div>
+            <div>
+                <h2> Contact </h2>
+                <a href="mailto:admin@gilgoblin.com">admin@gilgoblin.com</a>
+            </div>
         </div>
     );
 };
--- src/frontend/src/components/Results/Data/RecipeComponentTests.spec.tsx
+++ src/frontend/src/components/Results/Data/RecipeComponentTests.spec.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import RecipeComponent from './RecipeComponent';
 import {Recipe} from '../../../types/types';
-import ItemComponent from "./ItemComponent";
 
 describe('RecipeComponent', () => {
     const recipeData: Recipe =
@@ -14,36 +13,17 @@ describe('RecipeComponent', () => {
             resultQuantity: 1,
             canHq: true,
             canQuickSynth: true,
-            itemIngredient0TargetId: 111,
-            amountIngredient0: 11,
-            itemIngredient1TargetId: 222,
-            amountIngredient1: 22,
-            itemIngredient2TargetId: 0,
-            amountIngredient2: 0,
-            itemIngredient3TargetId: 0,
-            amountIngredient3: 0,
-            itemIngredient4TargetId: 0,
-            amountIngredient4: 0,
-            itemIngredient5TargetId: 0,
-            amountIngredient5: 0,
-            itemIngredient6TargetId: 0,
-            amountIngredient6: 0,
-            itemIngredient7TargetId: 0,
-            amountIngredient7: 0,
-            itemIngredient8TargetId: 0,
-            amountIngredient8: 0,
-            itemIngredient9TargetId: 333,
-            amountIngredient9: 33
         };
+
     const expectedLabels =
         {
             "id": `Recipe Id: ${recipeData.id}`,
             "craftType": `Craft Type: ${recipeData.craftType}`,
-            "recipeLevelTable" : `Recipe Level Table: ${recipeData.recipeLevelTable}`,
-            "targetItemId" : `Target Item Id: ${recipeData.targetItemId}`,
-            "resultQuantity" : `Result Quantity: ${recipeData.resultQuantity}`,
-            "canHq" : "Can Hq: Yes",
-            "canQuickSynth" : "Can QuickSynth: Yes"
+            "recipeLevelTable": `Recipe Level Table: ${recipeData.recipeLevelTable}`,
+            "targetItemId": `Target Item Id: ${recipeData.targetItemId}`,
+            "resultQuantity": `Result Quantity: ${recipeData.resultQuantity}`,
+            "canHq": "Can Hq: Yes",
+            "canQuickSynth": "Can QuickSynth: Yes"
         };
 
     Object.entries(expectedLabels)
@@ -56,4 +36,12 @@ describe('RecipeComponent', () => {
                 expect(fieldElement).toBeInTheDocument();
             });
         });
+
+    it('renders ingredient components', async () => {
+        render(<RecipeComponent recipe={recipeData} />);
+
+        const ingredientComponents = await screen.findAllByText('Ingredients')
+
+        expect(ingredientComponents).toHaveLength(1);
+    });
 });
