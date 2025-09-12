de2b773948b2692066a30b774b9c8de89c38780e
Nick Reinlein <nicholasreinlein@gmail.com>
2025-09-12 16:53:04 -0400

    reverse docker compose change for local testing

--- src/frontend/docker-entrypoint.sh
+++ src/frontend/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Use envsubst to replace the API_HOSTNAME placeholder
 envsubst '${API_HOSTNAME}' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
-# Call the wait-for-nginx script
 /wait-for-nginx.sh "$API_HOSTNAME" 55448 -- nginx -g "daemon off;"
