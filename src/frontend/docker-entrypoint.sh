86f921980d17539d0fc1ac453788dd921b5e92bb
Nick Reinlein <nicholasreinlein@gmail.com>
2025-09-12 18:46:01 -0400

    Give it time
    Signed-off-by: Nick Reinlein <nicholasreinlein@gmail.com>

--- src/frontend/docker-entrypoint.sh
+++ src/frontend/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 envsubst '${API_HOSTNAME}' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
+echo "Waiting for DNS record to propagate..."
+sleep 10 # Wait to give DNS a chance to register
+
 /wait-for-nginx.sh "$API_HOSTNAME" 55448 -- nginx -g "daemon off;"
