de7cb65ee9db96b6b97e759db77a75e22dd6ae77
Nick Reinlein <nicholasreinlein@gmail.com>
2025-09-13 15:34:05 -0600

    remove-the-while-loop-in-docker-entrypoint.sh (#210)
    Signed-off-by: Nick Reinlein <nicholasreinlein@gmail.com>

--- src/frontend/docker-entrypoint.sh
+++ src/frontend/docker-entrypoint.sh
@@ -3,12 +3,6 @@ set -e
 
 envsubst '${API_HOSTNAME}' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
-echo "Waiting for DNS record to resolve..."
-while ! ping -c 1 "$API_HOSTNAME" > /dev/null 2>&1; do
-echo "Still waiting for $API_HOSTNAME..."
-sleep 1
-done
-
-echo "DNS resolved. Starting Nginx..."
+echo "Starting Nginx..."
 
 exec nginx -g "daemon off;"
